e47e53aaf5eb1c7f74716cf5f9a3911e4b444426
cjgallon <65310145+cjgallon@users.noreply.github.com>
2023-11-10 18:01:47 -0500

    Add files via upload

---
+++ Taller1 - C#_Solid - Carlos Gallon/Taller1 - C#_Solid - Carlos Gallon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la cadena: ");
+        string cadena = Console.ReadLine();
+
+        Console.Write("Ingrese el tamaño: ");
+        int tamaño = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Combinaciones de {cadena} con tamaño {tamaño}:");
+
+        GenerarCombinaciones(cadena, tamaño, "");
+    }
+
+    static void GenerarCombinaciones(string cadena, int tamaño, string combinacionActual)
+    {
+        if (tamaño == 0)
+        {
+            // Se ha alcanzado el tamaño deseado, imprimir la combinación actual
+            Console.WriteLine(combinacionActual);
+            return;
+        }
+
+        for (int i = 0; i < cadena.Length; i++)
+        {
+            // Tomar un carácter de la cadena
+            char caracter = cadena[i];
+
+            // Generar combinaciones recursivamente con el resto de la cadena
+            GenerarCombinaciones(cadena.Remove(i, 1), tamaño - 1, combinacionActual + caracter);
+        }
+    }
+}
---
+++ Taller1 - C#_Solid - Carlos Gallon/Taller1 - C#_Solid - Carlos Gallon/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Taller1 - C#_Solid - Carlos Gallon")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Taller1 - C#_Solid - Carlos Gallon")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("4b5133d8-db56-4e1f-9380-a50081780a36")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
